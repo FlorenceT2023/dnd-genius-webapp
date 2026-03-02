786a0bae7ba7cc3236ef5cbdeab07214bb8cbebc
Hervé <46354420+hahleung-resilient@users.noreply.github.com>
2026-03-02 15:48:18 +0000

    DND-6 Install Naive UI

---
+++ src/components/DummySwitch.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { NSwitch } from 'naive-ui'
+
+import { ref } from 'vue'
+
+const active = ref(false)
+</script>
+
+<template>
+  <n-space>
+    <n-switch v-model:value="active" />
+  </n-space>
+</template>
+
+<style scoped></style>
