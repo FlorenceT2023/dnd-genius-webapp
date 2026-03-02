cf6b1a109764ed2e20f29b363c4035d9e80a86cc
lacefronte <lacefronte@gmail.com>
2026-03-02 12:51:12 -0330

    DND-1 Created search bar

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,16 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import DummySwitch from "./components/DummySwitch.vue";
+import SearchBar from './components/SearchBar.vue';
+import { NH1 } from 'naive-ui'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <n-h1 class="title">D&D Genius App</n-h1>
+  <SearchBar/>
 
-  <DummySwitch/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+.title { color: lightgray;}
+
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { NInput } from 'naive-ui'
+
+</script>
+
+<template>
+    <n-input size="large" round placeholder="Enter Spell" />
+</template>
+
+<style scoped>
+
+</style>
