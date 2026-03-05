b8734ea0add6f5b22b6b40098ef449aee855028c
lacefronte <lacefronte@gmail.com>
2026-03-05 12:09:52 -0330

    DND-9 Added Pinia as storage - Button now displays selected level number

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import naive from 'naive-ui'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(naive)
+app.use(pinia)
 
 app.mount('#app')
