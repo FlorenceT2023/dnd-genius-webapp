44f552f20b08e2af00dbfb1496d9802c68f2ee1c
lacefronte <lacefronte@gmail.com>
2026-03-05 12:09:52 -0330

    DND-9 Added Pinia as storage - Button now displays selected level number

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -2,36 +2,37 @@
 import { type DropdownOption } from 'naive-ui'
 import {ref} from 'vue'
 import type { JsonFormat, Spell } from '../models/jsonformat'
+import { useSpellsStore } from '../stores/spells'
 
 const spells = ref([] as Spell[])
 const spellsCount = ref(0)
+const spellStore = useSpellsStore()
 
 const options: DropdownOption[] = [
   {
     label: 'Level 1 Spells',
-    key: '1'
+    key: 1
   },
   {
     label: "Level 2 Spells",
-    key: '2'
+    key: 2
   },
   {
     label: 'Level 3 Spells',
-    key: '3'
+    key: 3
   },
   {
     label: 'Level 4 Spells',
-    key: '4'
+    key: 4
   }
 ]
 
 
-
-
 // handler when  user clicks a level spell option
-async function handleSelect(key: string | number) {
+async function handleSelect(key: number) {
   const response = await fetch(`https://www.dnd5eapi.co/api/2014/spells?level=${key}`)
   const result = (await response.json()) as JsonFormat
+  spellStore.selectLevel(key)
   console.log(result)
   spellsCount.value = result.count
   spells.value = result.results
@@ -45,7 +46,7 @@ async function handleSelect(key: string | number) {
     :options="options"
     @select="handleSelect"
   >
-    <n-button size="large" class="button" round>Search Spells by Level</n-button>
+    <n-button size="large" class="button" round>Level {{ spellStore.level }}</n-button>
   </n-dropdown>
 
 </template>
