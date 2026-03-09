6e489793d3177c0445afad4902a8899789b335c8
lacefronte <lacefronte@gmail.com>
2026-03-09 14:14:30 -0230

    DND-8 Created Search button and did some formatting

---
+++ src/components/SearchButton.vue
@@ -0,0 +1,14 @@
+   <script setup lang="ts">
+import { NButton } from 'naive-ui'
+
+</script>
+
+<template>
+<n-button size="large" ghost color="#c4c4c4" strong round>
+      Search
+    </n-button>
+</template>
+
+<style scoped>
+
+</style> 
