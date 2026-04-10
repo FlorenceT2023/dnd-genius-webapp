3d4a5152ba83b421d46d4216eb93f830f45cef13
lacefronte <lacefronte@gmail.com>
2026-04-10 14:39:57 -0230

    DND-21 Changed loading state using method from Pinia

--- src/components/SearchButton.vue
+++ src/components/SearchButton.vue
@@ -3,19 +3,34 @@ import type { JsonFormat } from '@/models/jsonformat';
 import { useSpellsStore } from '@/stores/spellsSearch';
 import { NButton } from 'naive-ui'
 import { ref } from 'vue';
+import { defineStore } from 'pinia'
 
 const spellStore = useSpellsStore()
 const loading = ref(false)
 
+export const useLoadingStore = defineStore('loading', {
+
+   state: () => {
+      return {
+         loading: false
+      }
+      
+   },
+   actions: {
+      changeState() {
+         this.loading = true
+      }
+   }
+})
 
 async function handleClick() {
-   loading.value = true;
+   useLoadingStore
    
-   await new Promise(resolve => setTimeout(() => {
-      loading.value = false
-      resolve(true)},
-      1500)
-   )
+   // await new Promise(resolve => setTimeout(() => {
+   //    loading.value = false
+   //    resolve(true)},
+   //    1500)
+   // )
 
    const response = await fetch(`http://localhost:8080/spells`)
    const result = (await response.json()) as JsonFormat
