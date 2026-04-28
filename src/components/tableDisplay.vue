09a2b3b3ea73f055efe9673e16d94e295ca8732e
Hervé <herveherveherve@aol.com>
2026-04-28 18:48:49 +0000

    DND-22 Add back style for table

--- src/components/tableDisplay.vue
+++ src/components/tableDisplay.vue
@@ -3,10 +3,11 @@ import { computed } from 'vue'
 import { NDataTable, NSpin } from 'naive-ui'
 import { useSpellsStore } from '@/stores/spellsSearch'
 import { useLoadingStore } from '@/stores/loading.ts'
-// TODO should rename the file from tableDisplay to TableDisplay
+
 const spellStore = useSpellsStore()
 const loadingStore = useLoadingStore()
 const data = computed(() => spellStore.spells)
+
 function createColumns() {
   return [
     {
@@ -15,24 +16,27 @@ function createColumns() {
     },
   ]
 }
+
 const columns = createColumns()
 const pagination = false as const
 const themeOverrides = {
+  DataTable: {
+    thColor: '#242424',
+    thTextColor: '#ffffff',
+    thFontWeight: '700',
     thFontSize: '22px',
     tdColor: '#242424',
     tdTextColor: '#ffffff',
     tdColorHover: '#575757',
-  }
-
+  },
+}
 </script>
 
 <template>
   <!-- Perhaps make the spell name the URL??? -->
-
-  <!-- 4 states to consider doing an wired component: zero state, loading state, working state, error state  -->
+  <!-- 4 states to consider doing an wired component: zero state, loading state, working state, error state  -->
   <!-- error state: https://www.naiveui.com/en-US/os-theme/components/result -->
   <!-- loading state: https://www.naiveui.com/en-US/os-theme/components/spin -->
-
   <n-config-provider :theme-overrides="themeOverrides">
     <n-spin v-if="loadingStore.loading" size="large" />
 
@@ -47,6 +51,10 @@ const themeOverrides = {
 </template>
 
 <style scoped>
+.button {
+  color: lightgray;
+}
+
 .rowHeader {
   font-size: medium;
 }
